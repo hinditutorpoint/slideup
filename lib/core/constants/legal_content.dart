b0f686717b202617a583403cd8cfb233e49c8c8d
hinditutorpoint <hinditutorpoint@gmail.com>
2026-08-17 20:47:33 +0530

    chore: replace all smplus.in references with GitHub repository and pages

--- lib/core/constants/legal_content.dart
+++ lib/core/constants/legal_content.dart
@@ -3,7 +3,8 @@ class LegalContent {
 
   static const String appName = 'Slideup Media Player';
   static const String companyName = 'Inkpad IT Solutions.';
-  static const String companyEmail = 'support@smplus.in';
+  static const String companyEmail =
+      'https://github.com/hinditutorpoint/slideup/issues';
   static const String companyWebsite =
       'https://hinditutorpoint.github.io/slideup/';
   static const String lastUpdated = 'January 1, 2025';
